795d5b67d8b5c636f7f7c6404771dbf19c4f9e34
vishalicious213 <vishalicious213@gmail.com>
2025-05-20 10:19:38 -0700

    drafted /layouts/PostLayout.astro

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,22 @@
 import { decode } from "entities"
 
+async function fixText(text) {
+    let replacements = {
+        "â€™": "'",
+        "/â€œ": '"',
+        "â€�": '"',
+        "â€“": "-",
+        "â€”": "-",
+        "â€¦": "…"
+    }
+
+    let newText = await text.replace(/â€™|â€œ|â€�|â€“|â€”|â€¦/g, (match) => {
+        return replacements[match];
+    })
+
+    return newText
+}
+
 export async function getPosts() {
     const response = await fetch("https://public-api.wordpress.com/wp/v2/sites/neophyte.home.blog/posts")
     
@@ -10,7 +27,7 @@ export async function getPosts() {
     const data = await response.json()
 
     // Decode title and excerpt for each post
-    const decodedPosts = data.map(post => ({
+    const decodedPosts = await data.map(post => ({
         ...post,
         title: {
             ...post.title,
@@ -20,7 +37,13 @@ export async function getPosts() {
             ...post.excerpt,
             rendered: decode(post.excerpt.rendered),
         },
+        content: {
+            ...post.content,
+            rendered: fixText(post.content.rendered),
+        }
     }))
+
+    // console.log(decodedPosts[3].content.rendered)
 
     return decodedPosts
 }
