83026eaf815a526a166ec4b4d6e997441565fb35
vishalicious213 <vishalicious213@gmail.com>
2025-05-26 18:34:15 -0700

    drafted /scripts/getPostTags.js

---
+++ src/scripts/getPostTags.js
@@ -0,0 +1,23 @@
+export async function getPostTags(postId) {
+    const response = await fetch(`https://public-api.wordpress.com/wp/v2/sites/neophyte.home.blog/posts/${postId}`)
+    const post = await response.json()
+
+    const categoryIds = post.categories.join(',')
+    const tagIds = post.tags.join(',')
+
+    const [categoriesRes, tagsRes] = await Promise.all([
+        fetch(`https://public-api.wordpress.com/wp/v2/sites/neophyte.home.blog/categories?include=${categoryIds}`),
+        fetch(`https://public-api.wordpress.com/wp/v2/sites/neophyte.home.blog/tags?include=${tagIds}`)
+    ])
+
+    const categories = await categoriesRes.json()
+    const tags = await tagsRes.json()
+
+    console.log("Categories", categories)
+    console.log("Tags", tags)
+
+    return {
+        categoryNames: categories.map(cat => cat.name),
+        tagNames: tags.map(tag => tag.name)
+    }
+}
